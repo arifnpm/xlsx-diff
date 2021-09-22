4fb292ef8ca45706e067acb650d6ef8fd0dd04dc
arifnpm <arif.masyur@candelalabs.io>
2021-09-23 01:44:13 +0700

    first release

---
+++ index.js
@@ -0,0 +1,26 @@
+const XLSX = require("xlsx");
+const jsondiffpatch = require("jsondiffpatch");
+const colorize = require('json-colorizer');
+
+let myArgs = process.argv.slice(2);
+if(myArgs.length<2) {
+	console.log("Usage: sheets_compare [file1] [file2]")
+}else{
+	const wb1 = XLSX.readFile(
+		myArgs[0]
+	);
+
+	const wb2 = XLSX.readFile(
+		myArgs[1]
+	);
+	
+	let delta = jsondiffpatch.diff(wb1, wb2);
+	const json = JSON.stringify(delta, null, 2);
+	console.log(colorize(json, {
+		colors: {
+		  STRING_KEY: 'blue',
+		  STRING_LITERAL: '#FFFF99',
+		  NUMBER_LITERAL: '#FF0000'
+		}
+	}));
+}
